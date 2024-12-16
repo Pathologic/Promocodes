ab87ec6cef7dd07bff38be5f1b1ae1534ebdc2b3
Pathologic <m@xim.name>
2024-12-16 10:25:57 +0300

    Update plugin.promocodes.php

--- assets/modules/promocodes/plugin.promocodes.php
+++ assets/modules/promocodes/plugin.promocodes.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'autoload.php';
+include_once MODX_BASE_PATH . 'assets/modules/promocodes/autoload.php';
 $plugin = new Pathologic\Commerce\Promocodes\Plugin($modx, $params);
 $event = $modx->event->name;
 if (method_exists($plugin, $event)) {
